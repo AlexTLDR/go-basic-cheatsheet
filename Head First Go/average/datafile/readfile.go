4aa7c9f6b385f8c7ef97b70c17f67ecd13d2effa
AlexFZR <abadragan@gmail.com>
2022-11-28 14:29:26 +0100

    improved error handling returns

--- Head First Go/average/datafile/readfile.go
+++ Head First Go/average/datafile/readfile.go
@@ -10,23 +10,23 @@ func GetFloats(fileName string) ([]float64, error) {
 	numbers := []float64{}
 	file, err := os.Open(fileName)
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		// fmt.Println(scanner.Text())
 		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
-			return numbers, err
+			return nil, err
 		}
 		numbers = append(numbers, number)
 	}
 	err = file.Close()
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 	if scanner.Err() != nil {
-		return numbers, scanner.Err()
+		return nil, scanner.Err()
 	}
 	return numbers, nil
 }
