ff90d4ee683d1039e9c7a2c701d03745fb95f569
AlexFZR <abadragan@gmail.com>
2022-11-28 12:11:50 +0100

    average with read file

---
+++ Head First Go/average/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	a "average/datafile"
+	"fmt"
+	"log"
+)
+
+func main() {
+	numbers, err := a.GetFloats("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	sampleCount := float64(len(numbers))
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+}
