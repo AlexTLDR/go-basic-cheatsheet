c3422837c753d3252a92be0851790802ddd88036
AlexFZR <abadragan@gmail.com>
2022-11-28 14:20:12 +0100

    removed the 3 row limit of the data.txt file

--- Head First Go/average/datafile/readfile.go
+++ Head First Go/average/datafile/readfile.go
@@ -6,21 +6,20 @@ import (
 	"strconv"
 )
 
-func GetFloats(fileName string) ([3]float64, error) {
-	numbers := [3]float64{}
+func GetFloats(fileName string) ([]float64, error) {
+	numbers := []float64{}
 	file, err := os.Open(fileName)
 	if err != nil {
 		return numbers, err
 	}
-	i := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		// fmt.Println(scanner.Text())
-		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64)
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
 			return numbers, err
 		}
-		i++
+		numbers = append(numbers, number)
 	}
 	err = file.Close()
 	if err != nil {
