97ae6767145403f98bec456af26ae4325f858764
AlexFZR <abadragan@gmail.com>
2022-12-01 13:24:04 +0100

    made the exported method's fileds unexported

--- Head First Go/encapsulation_and_embedding/setter_methods/calendar/date.go
+++ Head First Go/encapsulation_and_embedding/setter_methods/calendar/date.go
@@ -3,16 +3,16 @@ package calendar
 import "errors"
 
 type Date struct {
-	Year  int
-	Month int
-	Day   int
+	year  int
+	month int
+	day   int
 }
 
 func (d *Date) SetYear(year int) error {
 	if year < 1 {
 		return errors.New("invalid year")
 	}
-	d.Year = year
+	d.year = year
 	return nil
 }
 
@@ -20,7 +20,7 @@ func (d *Date) SetMonth(month int) error {
 	if month < 1 || month > 12 {
 		return errors.New("invalid month")
 	}
-	d.Month = month
+	d.month = month
 	return nil
 }
 
@@ -28,6 +28,6 @@ func (d *Date) SetDay(day int) error {
 	if day < 1 || day > 31 {
 		return errors.New("invalid month")
 	}
-	d.Day = day
+	d.day = day
 	return nil
 }
