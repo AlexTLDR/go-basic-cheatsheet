4bdf1f968a0152cf78cee996b12728322594995f
AlexFZR <abadragan@gmail.com>
2022-12-01 11:31:51 +0100

    added comments

--- Head First Go/encapsulation_and_embedding/main.go
+++ Head First Go/encapsulation_and_embedding/main.go
@@ -12,3 +12,5 @@ func main() {
 	date := Date{Year: 2022, Month: 12, Day: 1}
 	fmt.Println(date)
 }
+
+// Because users can enter invalid data, like Year: -1, Month: 13, Day: 45, in the encapsulating_and_embedding subfolder data validation will be used
