eb447e58f2eb0dd626107d561672adc45b64f945
AlexFZR <abadragan@gmail.com>
2022-12-01 13:15:56 +0100

    created calendar package

---
+++ Head First Go/encapsulation_and_embedding/setter_methods/calendar/date.go
@@ -0,0 +1,33 @@
+package calendar
+
+import "errors"
+
+type Date struct {
+	Year  int
+	Month int
+	Day   int
+}
+
+func (d *Date) SetYear(year int) error {
+	if year < 1 {
+		return errors.New("invalid year")
+	}
+	d.Year = year
+	return nil
+}
+
+func (d *Date) SetMonth(month int) error {
+	if month < 1 || month > 12 {
+		return errors.New("invalid month")
+	}
+	d.Month = month
+	return nil
+}
+
+func (d *Date) SetDay(day int) error {
+	if day < 1 || day > 31 {
+		return errors.New("invalid month")
+	}
+	d.Day = day
+	return nil
+}
--- Head First Go/encapsulation_and_embedding/setter_methods/main.go
+++ Head First Go/encapsulation_and_embedding/setter_methods/main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"errors"
+	"calendar/calendar"
 	"fmt"
 	"log"
 )
 
-type Date struct {
-	Year  int
-	Month int
-	Day   int
-}
-
 func main() {
-	date := Date{}
+	date := calendar.Date{}
 	err := date.SetYear(2022)
 	if err != nil {
 		log.Fatal(err)
@@ -27,28 +21,4 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(date)
-}
-
-func (d *Date) SetYear(year int) error {
-	if year < 1 {
-		return errors.New("invalid year")
-	}
-	d.Year = year
-	return nil
-}
-
-func (d *Date) SetMonth(month int) error {
-	if month < 1 || month > 12 {
-		return errors.New("invalid month")
-	}
-	d.Month = month
-	return nil
-}
-
-func (d *Date) SetDay(day int) error {
-	if day < 1 || day > 31 {
-		return errors.New("invalid month")
-	}
-	d.Day = day
-	return nil
 }
