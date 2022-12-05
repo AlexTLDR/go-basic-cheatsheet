a4df3a6c93115fe310eb2aabfacd604b3bddf30b
AlexFZR <abadragan@gmail.com>
2022-12-05 10:22:00 +0100

    switched to interface

--- Head First Go/interfaces/playList/main.go
+++ Head First Go/interfaces/playList/main.go
@@ -4,13 +4,18 @@ import (
 	"tapes/cassette"
 )
 
+type Player interface {
+	Play(string)
+	Stop()
+}
+
 func main() {
 	player := cassette.TapePlayer{}
 	mixTape := []string{"Viktor and his demons", "Mosca Cojonera", "Chop suey"}
 	playList(player, mixTape)
 }
 
-func playList(device cassette.TapePlayer, songs []string) {
+func playList(device Player, songs []string) {
 	for _, song := range songs {
 		device.Play(song)
 	}
