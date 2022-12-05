c1669bedbe8582d4e04a3695ef7925a7088bd523
AlexFZR <abadragan@gmail.com>
2022-12-05 11:26:59 +0100

    avoiding panics when type assertions fail

--- Head First Go/interfaces/playList/main.go
+++ Head First Go/interfaces/playList/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"tapes/cassette"
 )
 
@@ -16,7 +17,18 @@ func main() {
 	player = cassette.TapeRecorder{}
 	playList(player, mixTape)
 	TryOut(cassette.TapeRecorder{})
-
+	//avoiding panics when type assertions fail
+	/*
+		TryOut(cassette.TapePlayer{})
+		this will generate a panic -> panic: interface conversion: main.Player is cassette.TapePlayer, not cassette.TapeRecorder
+	*/
+	player = cassette.TapePlayer{}
+	recorder, ok := player.(cassette.TapeRecorder)
+	if ok {
+		recorder.Record()
+	} else {
+		fmt.Println("Player was not a TapeRecorder")
+	}
 }
 
 func playList(device Player, songs []string) {
