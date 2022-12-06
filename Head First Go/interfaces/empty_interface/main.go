342310a00a942f61f570c5250939e99783fa40ce
AlexFZR <abadragan@gmail.com>
2022-12-06 11:28:47 +0100

    added type assertion

--- Head First Go/interfaces/empty_interface/main.go
+++ Head First Go/interfaces/empty_interface/main.go
@@ -3,13 +3,29 @@ package main
 import "fmt"
 
 type Whistle string
+type MakeSound string
 
 func AcceptAnything(thing interface{}) {
 	fmt.Println(thing)
+	//call methods on a value with the empty interface -> use type assertion
+	whistle, ok := thing.(Whistle)
+	if ok {
+		whistle.MakeSound()
+	}
 }
 
 func (w Whistle) Whistle() {
 	fmt.Println(w)
+}
+
+func (w Whistle) MakeSound() {
+	fmt.Println(w)
+}
+
+// call methods on a value with the empty interface -> use type assertion
+func AcceptWhistle(whistle Whistle) {
+	fmt.Println(whistle)
+	whistle.MakeSound()
 }
 
 func main() {
