3f3e99ec737c290929d00d341ccd5c604f83a1a9
AlexFZR <abadragan@gmail.com>
2022-12-07 16:06:35 +0100

    goroutine synch initial commit

--- Head First Go/goroutines_and_channels/retrieving_web_pages/main.go
+++ Head First Go/goroutines_and_channels/retrieving_web_pages/main.go
@@ -5,13 +5,20 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
-	retrieveWebPage("https://example.com")
+	//go retrieveWebPage("https://example.com")
+	go responseSize("https://example.com")
+	go responseSize("https://google.com")
+	go responseSize("https://github.com")
+	go responseSize("https://gsp.ro")
+	time.Sleep(5 * time.Second)
 }
 
 func retrieveWebPage(url string) {
+	fmt.Println("Displaying", url)
 	response, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -22,4 +29,18 @@ func retrieveWebPage(url string) {
 		log.Fatal(err)
 	}
 	fmt.Println(string(body))
+}
+
+func responseSize(url string) {
+	fmt.Println("Getting", url)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(len(body))
 }
