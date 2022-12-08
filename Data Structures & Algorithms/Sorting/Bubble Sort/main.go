8169452e543de15e9eefbdc986d263ad799556ef
AlexFZR <abadragan@gmail.com>
2022-12-08 13:03:51 +0100

    no more swap improvement

--- Data Structures & Algorithms/Sorting/Bubble Sort/main.go
+++ Data Structures & Algorithms/Sorting/Bubble Sort/main.go
@@ -13,6 +13,21 @@ func BubbleSort(arr []int, comp func(int, int) bool) {
 	}
 }
 
+// when there is no more swap in one pass of the outer loop, the array is already sorted
+func BubbleSort2(arr []int, comp func(int, int) bool) {
+	size := len(arr)
+	swapped := 1
+	for i := 0; i < (size-1) && swapped == 1; i++ {
+		swapped = 0
+		for j := 0; j < size-i-1; j++ {
+			if comp(arr[j], arr[j+1]) {
+				arr[j+1], arr[j] = arr[j], arr[j+1]
+				swapped = 1
+			}
+		}
+	}
+}
+
 func Less(a, b int) bool {
 	return a < b
 }
@@ -23,6 +38,10 @@ func Greater(a, b int) bool {
 
 func main() {
 	data := []int{9, 1, 8, 2, 7, 3, 6, 4, 5}
-	BubbleSort(data, Greater)
+	BubbleSort(data, Greater) // to sort the data ascending
+	fmt.Println(data)
+	BubbleSort(data, Less) // to sort data descending
+	fmt.Println(data)
+	BubbleSort2(data, Greater)
 	fmt.Println(data)
 }
