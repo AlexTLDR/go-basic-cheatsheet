4faead59021bffda993226a8663a45a8701bf863
AlexFZR <abadragan@gmail.com>
2023-04-19 22:20:30 +0200

    mapped the languages

--- tiny projects/hello world/helloworld.go
+++ tiny projects/hello world/helloworld.go
@@ -11,14 +11,21 @@ func main() {
 
 type language string
 
+var phrasebook = map[language]string{
+	"el": "Χαίρετε Κόσμε",     // Greek
+	"en": "Hello world",       // English
+	"fr": "Bonjour le monde",  // French
+	"he": "שלום עולם",         // Hebrew
+	"ur": "ہیلو دنیا",         // Urdu
+	"vi": "Xin chào Thế Giới", // Vietnamese
+	"ro": "Salut lume",        // Romanian
+}
+
 // greet returns a greeting to the world.
 func greet(l language) string {
-	switch l {
-	case "en":
-		return "Hello world"
-	case "fr":
-		return "Bonjour le monde"
-	default:
-		return ""
+	greeting, ok := phrasebook[l]
+	if !ok {
+		return fmt.Sprintf("unsupported language: %q", l)
 	}
+	return greeting
 }
