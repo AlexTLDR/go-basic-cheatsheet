7432429f531306576460975d43b8943e57d35c65
AlexTLDR <alex.badragan@protonmail.com>
2024-05-18 13:26:15 +0200

    interfaces postgres

--- interfaces/main.go
+++ interfaces/main.go
@@ -15,6 +15,19 @@ type MongoDBNumberStore struct {
 	// some values
 }
 
+type PostgresNumberStorer struct {
+	// some postgres stuff
+}
+
+func (p PostgresNumberStorer) GetAll() ([]int, error) {
+	return []int{10, 20, 30}, nil
+}
+
+func (p PostgresNumberStorer) Put(number int) error {
+	fmt.Println("store the numbers into postgres storage")
+	return nil
+}
+
 func (m MongoDBNumberStore) GetAll() ([]int, error) {
 	return []int{1, 2, 3}, nil
 }
@@ -25,12 +38,29 @@ func (m MongoDBNumberStore) Put(number int) error {
 }
 
 func main() {
-	apiServer := ApiServer{
+	apiServerMongo := ApiServer{
 		numberStore: MongoDBNumberStore{},
 	}
 
-	numbers, err := apiServer.numberStore.GetAll()
+	numbers, err := apiServerMongo.numberStore.GetAll()
 	if err != nil {
+		panic(err)
+	}
+	if err = apiServerMongo.numberStore.Put(1); err != nil {
+		panic(err)
+	}
+	fmt.Println(numbers)
+
+	apiServerPostgres := ApiServer{
+		numberStore: PostgresNumberStorer{},
+	}
+
+	numbers, err = apiServerPostgres.numberStore.GetAll()
+	if err != nil {
+		panic(err)
+	}
+
+	if err = apiServerPostgres.numberStore.Put(1); err != nil {
 		panic(err)
 	}
 	fmt.Println(numbers)
