c341a9b9eafeb74208a2018e604760e272c8c76c
AlexTLDR <alex.badragan@protonmail.com>
2024-05-21 15:47:11 +0200

    interface embedding

--- interfaces/advanced-interfaces-mechanics/main.go
+++ interfaces/advanced-interfaces-mechanics/main.go
@@ -1,17 +1,58 @@
 package main
 
-type Storage interface {
-	Get(id int) (any, error)
+import "fmt"
+
+type Putter interface {
 	Put(id int, val any) error
+	fmt.Stringer
+}
+
+type Storage interface {
+	Putter
+	Get(id int) (any, error)
+}
+
+type simplePutter struct{}
+
+func (s *simplePutter) Put(id int, val any) error {
+	return nil
+}
+
+func (*simplePutter) String() string { return "" }
+
+type FooStorage struct{}
+
+func (s *FooStorage) Get(id int) (any, error) {
+	return nil, nil
+}
+
+func (s *FooStorage) Put(id int, val any) error {
+	return nil
+}
+
+type BarStorage struct{}
+
+func (s *BarStorage) Get(id int) (any, error) {
+	return nil, nil
+}
+
+func (s *BarStorage) Put(id int, val any) error {
+	return nil
 }
 
 type Server struct {
 	store Storage
 }
 
+func updateValue(id int, val any, p Putter) error {
+	return p.Put(id, val)
+}
+
 func main() {
 
-	s := &Server{}
-	s.store.Get(1)
-	s.store.Put(1, "foo")
+	// s := &Server{
+	// 	store: &FooStorage{},
+	// }
+	sputter := &simplePutter{}
+	updateValue(1, "foo", sputter)
 }
