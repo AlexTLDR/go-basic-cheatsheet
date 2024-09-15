d9e0e00f946201cbb17bb7616c31b6851eaa29b7
AlexTLDR <alex.badragan@protonmail.com>
2024-09-15 18:53:20 +0200

    errors and interfaces

--- interfaces/geometry/main.go
+++ interfaces/geometry/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Shape interface {
 	Area() float64
@@ -37,4 +40,22 @@ func describeShape(g Geometry) {
 func main() {
 	rect := Rectangle{Width: 5, Height: 4}
 	describeShape(rect)
+	val := 25.0
+	result, err := performCalculation(val)
+	fmt.Printf("Square root of %v is %v and error is %v\n", val, result, err)
+}
+
+type CalculationError struct {
+	msg string
+}
+
+func (ce CalculationError) Error() string {
+	return ce.msg
+}
+
+func performCalculation(val float64) (float64, error) {
+	if val < 0 {
+		return 0, CalculationError{"Value cannot be negative"}
+	}
+	return math.Sqrt(val), nil
 }
